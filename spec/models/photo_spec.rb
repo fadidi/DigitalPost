eb132be7fa45c41ffa7c1f8f48ac60758d442a03
John F. Brown <jack@brownjohnf.com>
2012-10-09 07:56:24 +0000

    working on photo validations

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -3,7 +3,14 @@ require 'spec_helper'
 describe Photo do
   before :each do
     @attr = {
-      :title => 'Test Photo'
+      :title => 'Test Photo',
+      :photo => 'photo',
+      :height => 1,
+      :width => 1,
+      :user_id => 1,
+      :photo_file_size => 1,
+      :photo_content_type => 'test',
+      :photo_hash => 'sthoeua'
     }
   end
 
@@ -18,6 +25,7 @@ describe Photo do
   it {should respond_to :photo}
   it {should respond_to :photo_content_type}
   it {should respond_to :photo_file_size}
+  it {should respond_to :photo_hash}
   it {should respond_to :title}
   it {should respond_to :user_id}
   it {should respond_to :width}
@@ -33,6 +41,7 @@ describe Photo do
     it {Photo.new.photo.should be_blank}
     it {Photo.new.photo_content_type.should be_blank}
     it {Photo.new.photo_file_size.should be_blank}
+    it {Photo.new.photo_hash.should be_blank}
     it {Photo.new.title.should be_blank}
     it {Photo.new.user_id.should be_blank}
     it {Photo.new.width.should be_blank}
@@ -40,5 +49,27 @@ describe Photo do
 
   describe 'validations' do
     it {Photo.new(@attr.merge(:attribution => '')).should be_valid}
+    it {Photo.new(@attr.merge(:attribution => 'a'*256)).should_not be_valid}
+    it {Photo.new(@attr.merge(:description => '')).should be_valid}
+    it {Photo.new(@attr.merge(:description => 'a'*256)).should be_valid}
+    it {Photo.new(@attr.merge(:height => '')).should_not be_valid}
+    it {Photo.new(@attr.merge(:height => 'a')).should_not be_valid}
+    it {Photo.new(@attr.merge(:imageable_id => '')).should be_valid}
+    it {Photo.new(@attr.merge(:imageable_id => 'a')).should_not be_valid}
+    it {Photo.new(@attr.merge(:imageable_type => '')).should be_valid}
+    it {Photo.new(@attr.merge(:imageable_type => 'a'*256)).should_not be_valid}
+    it {Photo.new(@attr.merge(:photo => '')).should_not be_valid}
+    it {Photo.new(@attr.merge(:photo_content_type => '')).should_not be_valid}
+    it {Photo.new(@attr.merge(:photo_content_type => 'a'*256)).should_not be_valid}
+    it {Photo.new(@attr.merge(:photo_file_size => '')).should_not be_valid}
+    it {Photo.new(@attr.merge(:photo_file_size => 'a')).should_not be_valid}
+    it {Photo.new(@attr.merge(:photo_hash => '')).should_not be_valid}
+    it {Photo.new(@attr.merge(:photo_hash => 'a'*256)).should_not be_valid}
+    it {Photo.new(@attr.merge(:title => '')).should be_valid}
+    it {Photo.new(@attr.merge(:title => 'a'*256)).should_not be_valid}
+    it {Photo.new(@attr.merge(:user_id => '')).should_not be_valid}
+    it {Photo.new(@attr.merge(:user_id => 'a')).should_not be_valid}
+    it {Photo.new(@attr.merge(:width => '')).should_not be_valid}
+    it {Photo.new(@attr.merge(:width => 'a')).should_not be_valid}
   end
 end
